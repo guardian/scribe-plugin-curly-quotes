3cbeb851d06bd0b3b0699b8157a9ac89c3418f6f
Oliver Joseph Ash <oliverjash@icloud.com>
2014-07-30 11:14:53 +0100

    Add missing path

--- Plumbing.js
+++ Plumbing.js
@@ -24,6 +24,7 @@ module.exports = function (pipelines) {
       // FIXME: auto?
       preserveLicenseComments: false,
       paths: {
+        'scribe-common': '../bower_components/scribe-common',
         'lodash-amd': '../bower_components/lodash-amd',
       }
     }),
