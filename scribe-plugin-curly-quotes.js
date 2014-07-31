76c05f98125f8e4f179016c38fb9dea181230640
Oliver Joseph Ash <oliverjash@icloud.com>
2014-07-31 16:32:57 +0100

    Update distribution files

--- scribe-plugin-curly-quotes.js
+++ scribe-plugin-curly-quotes.js
@@ -1063,7 +1063,84 @@ define('scribe-common/src/element',['lodash-amd/modern/collections/contains'], f
 
 });
 
-define('scribe-plugin-curly-quotes',['scribe-common/src/element'], function (element) {
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="amd" -o ./modern/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+define('lodash-amd/modern/objects/values',['./keys'], function(keys) {
+
+  /**
+   * Creates an array composed of the own enumerable property values of `object`.
+   *
+   * @static
+   * @memberOf _
+   * @category Objects
+   * @param {Object} object The object to inspect.
+   * @returns {Array} Returns an array of property values.
+   * @example
+   *
+   * _.values({ 'one': 1, 'two': 2, 'three': 3 });
+   * // => [1, 2, 3] (property order is not guaranteed across environments)
+   */
+  function values(object) {
+    var index = -1,
+        props = keys(object),
+        length = props.length,
+        result = Array(length);
+
+    while (++index < length) {
+      result[index] = object[props[index]];
+    }
+    return result;
+  }
+
+  return values;
+});
+
+/**
+ * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
+ * Build: `lodash modularize modern exports="amd" -o ./modern/`
+ * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
+ * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
+ * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+ * Available under MIT license <http://lodash.com/license>
+ */
+define('lodash-amd/modern/collections/toArray',['../objects/isString', '../internals/slice', '../objects/values'], function(isString, slice, values) {
+
+  /**
+   * Converts the `collection` to an array.
+   *
+   * @static
+   * @memberOf _
+   * @category Collections
+   * @param {Array|Object|string} collection The collection to convert.
+   * @returns {Array} Returns the new converted array.
+   * @example
+   *
+   * (function() { return _.toArray(arguments).slice(1); })(1, 2, 3, 4);
+   * // => [2, 3, 4]
+   */
+  function toArray(collection) {
+    if (collection && typeof collection.length == 'number') {
+      return slice(collection);
+    }
+    return values(collection);
+  }
+
+  return toArray;
+});
+
+define('scribe-plugin-curly-quotes',[
+  'scribe-common/src/element',
+  'lodash-amd/modern/collections/toArray'
+], function (
+  elementHelpers,
+  toArray
+) {
 
   
 
@@ -1101,7 +1178,7 @@ define('scribe-plugin-curly-quotes',['scribe-common/src/element'], function (ele
         if (curlyQuoteChar) {
           var selection = new scribe.api.Selection();
           var containingBlockElement = scribe.allowsBlockElements()
-            ? selection.getContaining(element.isBlockElement)
+            ? selection.getContaining(elementHelpers.isBlockElement)
             : scribe.el;
 
           selection.placeMarkers();
@@ -1127,19 +1204,24 @@ define('scribe-plugin-curly-quotes',['scribe-common/src/element'], function (ele
 
         // Replace straight single and double quotes with curly
         // equivalent in the given string
-        mapTextNodes(holder, function(str) {
+        mapElements(holder, function(prev, str) {
           // Tokenise HTML elements vs text between them
           // Note: this is escaped HTML in the text node!
           // Split by elements
-          var tokens = str.split(/(<[^>]+?>(?:.*<\/[^>]+?>)?)/);
-          return tokens.map(function(token) {
-            // Only replace quotes in text between (potential) HTML elements
-            if (/^</.test(token)) {
-              return token;
-            } else {
-              return convert(token);
-            }
-          }).join('');
+          // We tokenise with the previous text nodes for context, but
+          // only extract the current text node.
+          var tokens = (prev + str).split(/(<[^>]+?>(?:.*<\/[^>]+?>)?)/);
+          return tokens
+            .map(function(token) {
+              // Only replace quotes in text between (potential) HTML elements
+              if (/^</.test(token)) {
+                return token;
+              } else {
+                return convert(token);
+              }
+            })
+            .join('')
+            .slice(prev.length);
         });
 
         return holder.innerHTML;
@@ -1154,43 +1236,57 @@ define('scribe-plugin-curly-quotes',['scribe-common/src/element'], function (ele
         } else {
           var foo = str.
             // Use [\s\S] instead of . to match any characters _including newlines_
-            replace(/([\s\S])?'([\s\S])?/,
+            replace(/([\s\S])?'/,
                     replaceQuotesFromContext(openSingleCurly, closeSingleCurly)).
-            replace(/([\s\S])?"([\s\S])?/,
+            replace(/([\s\S])?"/,
                     replaceQuotesFromContext(openDoubleCurly, closeDoubleCurly));
           return convert(foo);
         }
       }
 
       function replaceQuotesFromContext(openCurly, closeCurly) {
-        return function(m, prev, next) {
+        return function(m, prev) {
           prev = prev || '';
-          next = next || '';
-          var isStart = ! prev;
-          var isEnd = ! next;
           var hasCharsBefore = isWordCharacter(prev);
-          var hasCharsAfter  = isWordCharacter(next);
           // Optimistic heuristic, would need to look at DOM structure
           // (esp block vs inline elements) for more robust inference
-          if (hasCharsBefore || (isStart && ! hasCharsAfter && ! isEnd)) {
-            return prev + closeCurly + next;
+          if (hasCharsBefore) {
+            return prev + closeCurly;
           } else {
-            return prev + openCurly + next;
+            return prev + openCurly;
           }
         };
       }
 
       // Apply a function on all text nodes in a container, mutating in place
-      function mapTextNodes(container, func) {
-        var walker = document.createTreeWalker(container, NodeFilter.SHOW_TEXT);
-        var node = walker.firstChild();
-        if (node) {
-          do {
-            node.data = func(node.data);
-          } while ((node = walker.nextSibling()));
+      function mapElements(containerElement, func) {
+        // TODO: This heuristic breaks for elements that contain a mixture of
+        // inline and block elements.
+        var nestedBlockElements = toArray(containerElement.children).filter(elementHelpers.isBlockElement);
+        if (nestedBlockElements.length) {
+          nestedBlockElements.forEach(function (nestedBlockElement) {
+            // Map the nested block elements
+            mapElements(nestedBlockElement, func);
+          });
+        } else {
+          mapTextNodes(containerElement, func);
         }
+      }
 
-        return node;
+      function mapTextNodes(containerElement, func) {
+        // TODO: Only walk inside of text nodes within inline elements
+        var walker = document.createTreeWalker(containerElement, NodeFilter.SHOW_TEXT);
+        var node = walker.firstChild();
+        var prevTextNodes = '';
+        while (node) {
+          // Split by BR
+          if (node.previousSibling && node.previousSibling.nodeName === 'BR') {
+            prevTextNodes = '';
+          }
+          node.data = func(prevTextNodes, node.data);
+          prevTextNodes += node.data;
+          node = walker.nextSibling();
+        }
       }
 
     };
