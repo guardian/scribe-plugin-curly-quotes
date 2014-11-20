ab0053709a0b26c82283003cda53885a3b79be73
Robert Rees <robert.rees@guardian.co.uk>
2014-11-20 22:40:02 +0000

    Check whether the chromedriver check is causing the build to die

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ then
 	https://selenium-release.storage.googleapis.com/$SELENIUM_MINOR_VERSION/selenium-server-standalone-$SELENIUM_VERSION.jar
 fi
 
-command -v chromedriver >/dev/null 2>&1 || echo 'Please install chromedriver for your os'; exit 1;
+#command -v chromedriver >/dev/null 2>&1 || echo 'Please install chromedriver for your os'; exit 1;
