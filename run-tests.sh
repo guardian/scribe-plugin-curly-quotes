a8fba54aebcc4f549bd87be572cabf7272ad2138
Robert Rees <robert.rees@guardian.co.uk>
2015-10-08 12:58:47 +0100

    Updates the Travis matrix and reverts the run-tests.sh script which might have been a red-herring

--- run-tests.sh
+++ run-tests.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-export BROWSER_NAME=${BROWSER_NAME:=chrome}
+export BROWSER_NAME=${BROWSER_NAME:-chrome}
+export TEST_SERVER_PORT=${TEST_SERVER_PORT:-3333}
 
-export TEST_SERVER_PORT=${TEST_SERVER_PORT:=8880}
-
-./node_modules/scribe-test-harness/node_modules/.bin/webdriver-manager update;
-
-./node_modules/scribe-test-harness/node_modules/.bin/http-server -p $TEST_SERVER_PORT --silent &
-HTTP_PID=$!
-
-./node_modules/scribe-test-harness/node_modules/.bin/webdriver-manager start > /dev/null &
-DRIVER_PID=$!
+./node_modules/.bin/http-server -p $TEST_SERVER_PORT --silent &
+PID=$!
 
 node test/integration/runner
 TEST_RUNNER_EXIT=$?
-
-kill $HTTP_PID
-#kill $DRIVER_PID
+kill $PID
 
 if [ $TEST_RUNNER_EXIT == "0" ]; then
     exit 0
 else
     exit 1
 fi
-
-
